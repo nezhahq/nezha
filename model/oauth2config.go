dfd965ebe9c3c959c4cca30e9abadec7a4340bd4
小辣鸡 <51939988+onexiaolaji@users.noreply.github.com>
2025-01-02 21:16:19 +0800

    Update oauth2config.go
    QQ Oauth2授权特殊地方。https://graph.qq.com/oauth2.0/me 需携带fmt=json，正则匹配名称QQ/qq/Qq/qQ

--- model/oauth2config.go
+++ model/oauth2config.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"fmt"
 	"golang.org/x/oauth2"
+	"regexp"
+	"strings"
 )
 
 type Oauth2Config struct {
@@ -30,4 +33,45 @@ func (c *Oauth2Config) Setup(redirectURL string) *oauth2.Config {
 		RedirectURL: redirectURL,
 		Scopes:      c.Scopes,
 	}
+}
+
+// AddFmtParamIfNeeded 添加 fmt 参数到 URL 如果授权方式为 QQ
+func (c *Oauth2Config) AddFmtParamIfNeeded(authMethod string) string {
+	// 正则表达式匹配 QQ 相关的授权方式
+	re := regexp.MustCompile(`(?i)qq`)
+	if re.MatchString(authMethod) {
+		// 如果匹配，则添加 fmt=json 参数
+		return fmt.Sprintf("%s&fmt=json", c.UserInfoURL)
+	}
+	// 如果不匹配，则返回原始 URL
+	return c.UserInfoURL
+}
+
+// GetUserInfo 使用 access_token 获取用户信息，并根据需要添加 fmt 参数
+func GetUserInfo(access_token, openid, authMethod string) (string, error) {
+	// 获取配置实例，这里假设您已经有了一个全局的配置实例
+	config := &Oauth2Config{
+		// 填充您的配置信息
+	}
+	urlWithFmt := config.AddFmtParamIfNeeded(authMethod)
+
+	// 发起请求获取用户信息
+	response, err := http.Get(urlWithFmt)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	// 检查响应状态码
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to get user info, status code: %d", response.StatusCode)
+	}
+
+	// 读取响应内容
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(contents), nil
 }
