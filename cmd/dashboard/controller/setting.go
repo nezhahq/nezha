bbbcfc520473e2a8aefd8bfa6937181597c0290a
uubulb <uub@kuzu.uk>
2025-03-17 13:54:33 +0800

    ddns: retreive dns servers from context

--- cmd/dashboard/controller/setting.go
+++ cmd/dashboard/controller/setting.go
@@ -107,7 +107,6 @@ func updateConfig(c *gin.Context) (any, error) {
 		return nil, newGormError("%v", err)
 	}
 
-	singleton.OnNameserverUpdate()
 	singleton.OnUpdateLang(singleton.Conf.Language)
 	return nil, nil
 }
